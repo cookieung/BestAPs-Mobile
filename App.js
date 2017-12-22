e9f7a48909544c53b5e25785c2be6700cc7e7c94
Salilthip Phuklang <ung_1002@hotmail.com>
2017-12-22 17:35:21 +0700

    Can pass the username and password to mainpage

--- App.js
+++ App.js
@@ -9,7 +9,11 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  TextInput,
+  Button,
+  ScrollView,
+  Alert
 } from 'react-native';
 
 const instructions = Platform.select({
@@ -19,21 +23,111 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-export default class App extends Component<{}> {
+class Secured extends Component {
+  userLogout(e) {
+      this.props.onLogout();
+      e.preventDefault();
+  }
+   
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Here is change!!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+      return (
+          <ScrollView style={{padding: 20}}>
+              <Text style={{fontSize: 27}}>
+                  {`Welcome ${this.props.username}`}
+              </Text>
+              <View style={{margin: 20}}/>
+              <Button onPress={(e) => this.userLogout(e)} title="Logout"/>
+          </ScrollView>
+      );
+  }
+}
+
+class Login extends Component {
+  constructor (props) {
+      super(props);
+      this.state = {
+          route: 'Login',
+          username: '',
+          password: ''
+      };
+  }
+
+	componentWillUpdate(nextProps, nextState) {
+		if (this.state != nextState)
+      this.props.handleDataLogin(nextState.username, nextState.password);
+	}
+
+
+  userLogin(e){
+    this.props.onLogin();
+  }
+
+  toggleRoute(e){
+    let alt = (this.state.route === 'Login') ? 'SignUp' : 'Login';
+    this.setState({ route: alt });
+    e.preventDefault();
+  }
+
+  render () {
+      let alt = (this.state.route === 'Login') ? 'SignUp' : 'Login';
+      return (
+          <ScrollView style={{padding: 20}}>
+              <Text style={{fontSize: 27}}>{this.state.route}</Text>
+              <TextInput 
+                  placeholder='Username'
+                  autoCapitalize='none'
+                  autoCorrect={false} 
+                  autoFocus={true} 
+                  keyboardType='email-address'
+                  value={this.state.username} 
+                  onChangeText={(text) => this.setState({ username: text })} />
+              <TextInput 
+                  placeholder='Password'
+                  autoCapitalize='none'
+                  autoCorrect={false} 
+                  secureTextEntry={true} 
+                  value={this.state.password} 
+                  onChangeText={(text) => this.setState({ password: text })} />
+              <View style={{margin: 7}}/>
+              <Button onPress={(e) => this.userLogin(e)} title={this.state.route}/>
+              <Text style={{fontSize: 16, color: 'blue'}} onPress={(e) => this.toggleRoute(e)}>{alt}</Text>
+          </ScrollView>
+      );
+  }
+
+
+}
+
+
+
+export default class App extends Component<{}> {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      isLogin : false,
+      username:'',
+      password: ''
+    }
+  }
+
+  handleLogin(name,pass){
+    this.setState({
+      username : name,
+      password: pass
+    });
+    Alert.alert("handle Login"+ this.state.username);
+  }
+
+  render() {
+    if (this.props.isLoggedIn) {
+      return <Secured />;
+    } else {
+      return <Login 
+      isSuccess={this.state.isLogin} 
+      handleDataLogin={(username,password) => this.setState({username: username,password: password})}
+      onLogin={(username,password) => this.handleLogin(username,password)}/>;
+    }
   }
 }
 
